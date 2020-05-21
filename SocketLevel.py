56b734c47a5b0e87806d1c8144f3a263299ee5dd
Misipuk <maxtretyakov37@gmail.com>
2020-05-21 21:54:26 +0300

    Great Work

--- SocketLevel.py
+++ SocketLevel.py
@@ -1,20 +1,39 @@
+import datetime
+
 from UserRequestsFile import UserRequest
 
-#User
-#get users vverhu
-#get cafes
-#post review
+# User
+# get users vverhu
+# get cafes
+# post review
 # create cafe
 
-#CafeOwner
+# CafeOwner
 # UserFunctions
 # Edit functions
-#MultiThreading
+# MultiThreading
 
+
+login = "l"
 
 userRequest = UserRequest()
-userRequest.register("l", "p")
-userRequest.login("l", "p")
-userRequest.getCafes("l", "p")
+userRequest.register(login, "p")
+at = userRequest.login(login, "p")
+userRequest.add_cafe(at, {
+    "owner": login,
+    "name": "2",
+    "des": "3",
+    "city": "4",
+})
 
-#userRequest.do_smth()
+userRequest.get_users(at)
+
+userRequest.add_cafe_media(at, 1, "/home/dbaynak/2020-04-28-144316_364x393_scrot.png")
+userRequest.add_cafe_review(at, {
+    "owner": login,
+    "cafe_id": "2",
+    "stars": "3",
+    "description": "5",
+})
+
+# userRequest.do_smth()
--- UserRequestsFile.py
+++ UserRequestsFile.py
@@ -1,26 +1,128 @@
 import json
 import socket
+from typing import Dict
+
+PORT = 9091
+
 
 class UserRequest:
 
-    def getCafes(self): #with Reviews
-        pass
+    def getCafes(self):  # with Reviews
+        client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client_sock.connect(('localhost', PORT))
+        client_sock.sendall(b'GET /cafes HTTP/1.1 \n')
+        client_sock.sendall(b'Host: MyServer\n')
+        client_sock.sendall(b'Accept: application/json\n')
+        client_sock.sendall(
+            b'Authorization: eyJsb2dpbiI6ICJsIiwgImV4cGlyZSI6IDE1ODk4Nzk0NjMsICJrZXkiOiAieHFrMmVJamJ3eFJzdDVtd3JaLVRHZnJvZTctUzF4ZXEzTzNxSW9CN0JWQT0ifQ==\n')
+        client_sock.sendall(b'\n')
+        data = client_sock.recv(1024)
+        client_sock.close()
+        print('Received', repr(data))
+
+    def getCafesMedia(self):  # with Reviews
+        client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client_sock.connect(('localhost', PORT))
+        client_sock.sendall(b'GET /cafemedia HTTP/1.1 \n')
+        client_sock.sendall(b'Host: MyServer\n')
+        client_sock.sendall(b'Accept: application/json\n')
+        client_sock.sendall(
+            b'Authorization: eyJsb2dpbiI6ICJsIiwgImV4cGlyZSI6IDE1ODk4Nzk0NjMsICJrZXkiOiAieHFrMmVJamJ3eFJzdDVtd3JaLVRHZnJvZTctUzF4ZXEzTzNxSW9CN0JWQT0ifQ==\n')
+        client_sock.sendall(b'\n')
+        data = client_sock.recv(1024)
+        client_sock.close()
+        print('Received', repr(data))
+
+    def add_cafe_media(self, auth_token: str, cafe_id: int, file_to_img: str):  # with Reviews
+        with open(file_to_img, mode='rb') as f:
+            body = f.read()
+
+        client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client_sock.connect(('localhost', PORT))
+        client_sock.sendall(b'POST /cafe/media?cafe_id=%d&type=photo HTTP/1.1 \n' % cafe_id)
+        client_sock.sendall(b'Host: MyServer\n')
+        client_sock.sendall(b'Accept: application/json\n')
+        client_sock.sendall(b'Authorization: %s\n' % auth_token.encode())
+        client_sock.sendall(b'Content-Length: %d\n' % len(body))
+        client_sock.sendall(b'\n')
+        client_sock.sendall(body)
+        client_sock.sendall(b'\n')
+        data = client_sock.recv(1024)
+        client_sock.close()
+        print('Received', repr(data))
+
+    def delCafeMedia(self):
+        client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client_sock.connect(('localhost', PORT))
+        client_sock.sendall(b'POST /delcafemedia HTTP/1.1 \n')
+        client_sock.sendall(b'Host: MyServer\n')
+        client_sock.sendall(b'Accept: application/json\n')
+        client_sock.sendall(
+            b'Authorization: eyJsb2dpbiI6ICJsIiwgImV4cGlyZSI6IDE1ODk4Nzk0NjMsICJrZXkiOiAieHFrMmVJamJ3eFJzdDVtd3JaLVRHZnJvZTctUzF4ZXEzTzNxSW9CN0JWQT0ifQ==\n')
+        client_sock.sendall(b'\n')
+        data = client_sock.recv(1024)
+        client_sock.close()
+        print('Received', repr(data))
+
+    def editCafe(self):
+        client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client_sock.connect(('localhost', PORT))
+        client_sock.sendall(b'POST /editcafe HTTP/1.1 \n')
+        client_sock.sendall(b'Host: MyServer\n')
+        client_sock.sendall(b'Accept: application/json\n')
+        client_sock.sendall(
+            b'Authorization: eyJsb2dpbiI6ICJsIiwgImV4cGlyZSI6IDE1ODk4Nzk0NjMsICJrZXkiOiAieHFrMmVJamJ3eFJzdDVtd3JaLVRHZnJvZTctUzF4ZXEzTzNxSW9CN0JWQT0ifQ==\n')
+        client_sock.sendall(b'\n')
+        data = client_sock.recv(1024)
+        client_sock.close()
+        print('Received', repr(data))
 
     def createCafe(self):
-        pass
+        client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client_sock.connect(('localhost', PORT))
+        client_sock.sendall(b'POST /createcafe HTTP/1.1 \n')
+        client_sock.sendall(b'Host: MyServer\n')
+        client_sock.sendall(b'Accept: application/json\n')
+        client_sock.sendall(
+            b'Authorization: eyJsb2dpbiI6ICJsIiwgImV4cGlyZSI6IDE1ODk4Nzk0NjMsICJrZXkiOiAieHFrMmVJamJ3eFJzdDVtd3JaLVRHZnJvZTctUzF4ZXEzTzNxSW9CN0JWQT0ifQ==\n')
+        client_sock.sendall(b'\n')
+        data = client_sock.recv(1024)
+        client_sock.close()
+        print('Received', repr(data))
 
-    def addReview(self): #with Reviews
-        pass
+    def add_cafe_review(self, auth_token: str, review: Dict[str, str]):  # with Reviews
+        body = json.dumps(review).encode()
 
-    def delReview(self):#byReviewID
-        pass
+        client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client_sock.connect(('localhost', PORT))
+        client_sock.sendall(b'POST /cafe/review HTTP/1.1 \n')
+        client_sock.sendall(b'Host: MyServer\n')
+        client_sock.sendall(b'Accept: application/json\n')
+        client_sock.sendall(b'Content-Length: %d\n' % len(body))
+        client_sock.sendall(b'Authorization: %s\n' % auth_token.encode())
+        client_sock.sendall(b'\n')
+        client_sock.sendall(body + b'\n')
+        client_sock.sendall(b'\n')
+        data = client_sock.recv(1024)
+        client_sock.close()
+        print('Received', repr(data))
 
-    def createCafe(self):
-        pass
+    def delReview(self):  # byReviewID
+        client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client_sock.connect(('localhost', PORT))
+        client_sock.sendall(b'POST /delreview HTTP/1.1 \n')
+        client_sock.sendall(b'Host: MyServer\n')
+        client_sock.sendall(b'Accept: application/json\n')
+        client_sock.sendall(
+            b'Authorization: eyJsb2dpbiI6ICJsIiwgImV4cGlyZSI6IDE1ODk4Nzk0NjMsICJrZXkiOiAieHFrMmVJamJ3eFJzdDVtd3JaLVRHZnJvZTctUzF4ZXEzTzNxSW9CN0JWQT0ifQ==\n')
+        client_sock.sendall(b'\n')
+        data = client_sock.recv(1024)
+        client_sock.close()
+        print('Received', repr(data))
 
     def register(self, login: str, password: str):
         client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        client_sock.connect(('localhost', 9091))
+        client_sock.connect(('localhost', PORT))
         body = json.dumps({"login": login, "password": password})
         client_sock.sendall(b'POST /users HTTP/1.1 \n')
         client_sock.sendall(b'Host: MyServer\n')
@@ -32,9 +134,9 @@ class UserRequest:
         client_sock.close()
         print('Received', repr(data))
 
-    def login(self, login: str, password: str):
+    def login(self, login: str, password: str) -> str:
         client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        client_sock.connect(('localhost', 9090))
+        client_sock.connect(('localhost', PORT))
         body = json.dumps({"login": login, "password": password})
         client_sock.sendall(b'POST /login HTTP/1.1 \n')
         client_sock.sendall(b'Host: MyServer\n')
@@ -45,15 +147,33 @@ class UserRequest:
         data = client_sock.recv(1024)
         client_sock.close()
         print('Received', repr(data))
+        # TODO
+        return "eyJsb2dpbiI6ICJsIiwgImV4cGlyZSI6IDE1OTAwODM5OTMsICJrZXkiOiAiRWpNcUVlUmpzMVpuMldvcDlSRkVyeE5ZcXBTclpGMURXcVpXaE9vNTE5QT0ifQ=="
 
-    def do_smth(self):
+    def get_users(self, auth_token: str):
         client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        client_sock.connect(('localhost', 9090))
+        client_sock.connect(('localhost', PORT))
         client_sock.sendall(b'GET /users HTTP/1.1 \n')
         client_sock.sendall(b'Host: MyServer\n')
         client_sock.sendall(b'Accept: application/json\n')
-        client_sock.sendall(
-            b'Authorization: eyJsb2dpbiI6ICJsIiwgImV4cGlyZSI6IDE1ODk4Nzk0NjMsICJrZXkiOiAieHFrMmVJamJ3eFJzdDVtd3JaLVRHZnJvZTctUzF4ZXEzTzNxSW9CN0JWQT0ifQ==\n')
+        client_sock.sendall(b'Authorization: %s\n' % auth_token.encode())
+        client_sock.sendall(b'\n')
+        data = client_sock.recv(1024)
+        client_sock.close()
+        print('Received', repr(data))
+
+    def add_cafe(self, auth_token: str, cafe: Dict[str, str]):
+        body = json.dumps(cafe).encode()
+
+        client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client_sock.connect(('localhost', PORT))
+        client_sock.sendall(b'POST /cafe?kek=kek HTTP/1.1 \n')
+        client_sock.sendall(b'Host: MyServer\n')
+        client_sock.sendall(b'Accept: application/json\n')
+        client_sock.sendall(b'Content-Length: %d\n' % len(body))
+        client_sock.sendall(b'Authorization: %s\n' % auth_token.encode())
+        client_sock.sendall(b'\n')
+        client_sock.sendall(body + b'\n')
         client_sock.sendall(b'\n')
         data = client_sock.recv(1024)
         client_sock.close()
